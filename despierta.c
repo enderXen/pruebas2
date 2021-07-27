9f434bb80c9e190eabeb324ebfad7efbdfbf29de
CristianPlaz <capc@azc.uam.mx>
2021-07-26 23:42:47 -0500

    commit desde VS 260721

---
+++ despierta.c
@@ -0,0 +1,13 @@
+
+#include <conio.h>
+#include <stdio.h>
+
+int main()
+{
+    printf( "El universo es una creación mental sostenida en la mente del todo" );
+
+
+    getch(); /* Pausa */
+
+    return 0;
+} 
